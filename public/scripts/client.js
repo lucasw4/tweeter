a57066f01067610a90747b81db210841b4994016
Lucas Sahota <lucaswade4@gmail.com>
2022-11-17 01:53:05 -0800

    Implement createTweetElement and test data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,50 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+console.log(typeof jQuery);
+
+const tweetData = {
+  user: {
+    name: "Newton",
+    avatars: "https://i.imgur.com/73hZDYK.png",
+    handle: "@SirIsaac",
+  },
+  content: {
+    text: "If I have seen further it is by standing on the shoulders of giants",
+  },
+  created_at: 1461116232227,
+};
+
+$(document).ready(function () {
+  const createTweetElement = function (tweetObj) {
+    const $tweet = $(`<article>
+      <header class="tweet-list">
+        <img class="tweet-avatar" src=${tweetObj.user["avatars"]} />
+        <h4 class="user-name">${tweetObj.user["name"]}</h4>
+        <h4 class="user-handle">${tweetObj.user["handle"]}</h4>
+      </header>
+      <div class="tweet-details">
+        <p>
+          ${tweetObj.content["text"]}
+        </p>
+      </div>
+      <footer class="tweet-footer">
+        <span class="date">${tweetObj["created_at"]}</span>
+        <div class="icons">
+          <a href="www.google.com"><i class="fa-solid fa-flag"></i></a>
+          <div class="space"></div>
+          <a href="www.google.com"><i class="fa-solid fa-retweet"></i></a>
+          <div class="space"></div>
+          <a href="www.google.com"><i class="fa-solid fa-heart"></i></a>
+          <div class="space"></div>
+        </div>
+      </footer>
+    </article>`);
+    return $tweet;
+  };
+
+  const $tweet = createTweetElement(tweetData);
+
+  console.log($tweet);
+  $(".display-tweet").append($tweet);
+});
